ea345c9ee5fb1b09cbad15c4f23de165e3bdd4ea
Aspire ES1-432 <Aspire ES1-432@Jhe-Jhe>
2018-11-17 12:25:04 +0700

    1st

---
+++ src/connection.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Aspire ES1-432
+ */
+public class connection {
+    
+    String connect= "jdbc:mysql://localhost/storage?user=root&password=";
+
+}
